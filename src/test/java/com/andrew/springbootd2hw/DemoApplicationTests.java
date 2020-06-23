7c25f6cb33393d0c568da9f4a1e954649e5d335e
John Yerger <johnyerger@johns-mbp.home>
2020-06-23 07:46:09 -0400

    push

---
+++ src/test/java/com/andrew/springbootd2hw/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andrew.springbootd2hw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
